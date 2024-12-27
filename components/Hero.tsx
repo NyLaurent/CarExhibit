e01a34a780808bf5f59ce8066481075c5fd0277d
NyLaurent <laulanyumbayire@gmail.com>
2024-12-27 15:48:48 -0600

    Hero section finished

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,9 +29,8 @@ const Hero = () => {
         <div className='hero__image-container'>
             <div className='hero__image'>
                 <Image src="/hero.png" alt='' fill className='object-contain'/>
+                </div>
                 <div className='hero__image-overlay'/>
-
-            </div>
         </div>
       
     </div>
