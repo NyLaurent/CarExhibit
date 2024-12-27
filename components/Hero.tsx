0a0e23138dc85852e8e853a349de9216614d1b56
NyLaurent <laulanyumbayire@gmail.com>
2024-12-27 15:18:36 -0600

    types folder created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,6 +26,13 @@ const Hero = () => {
     
 
         </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" alt='' fill className='object-contain'/>
+                <div className='hero__image-overlay'/>
+
+            </div>
+        </div>
       
     </div>
   )
