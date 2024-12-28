d45a03f6a9d8a3aaff1db91d7bf7c865dbeae40a
NyLaurent <laulanyumbayire@gmail.com>
2024-12-27 19:02:58 -0600

    highlighting the selected

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -49,13 +49,7 @@ const SearchManufacturer =
                     afterLeave={() => setQuery('')}
                     >
                         <Combobox.Options>
-                            {filteredManufacturers.length=== 0 && query !=="" && query !== "" ?(
-                                <Combobox.Option
-                                value={query}
-                                className="search-manufacturer__option"
-                                >
-                                Create "{query}"</Combobox.Option>
-                            ):(
+                            {
                                 filteredManufacturers.map((item)=>(
                                     <Combobox.Option
                                     key={item}
@@ -63,12 +57,30 @@ const SearchManufacturer =
                                     relative search-manufacturer__option ${active ? 'bg-primary-blue text-white':'text-gray-900'}
                                     `}
                                     value={item}>
-                                        {item}
+                                      {({selected,active})=>(
+                                        <>
+                                        <span className={`block truncate ${
+                                            selected ? 'font-medium' :
+                                            'font-normal'
+                                        }`}>
+                                            {item}
+                                        </span>
+                                        {selected ? (
+                                            <span className={`absolute inset-y-0 left-0 
+                                                flex items-center pl-3 ${
+                                                    active ? 'text-white' : 
+                                                    'text-teal-600'
+                                                }`}></span>
+                                        ) : null}
+                                        
+                                        </>
+
+                                )}
 
                                     </Combobox.Option>
 
                                 ))
-                            ) }
+                             }
 
                         </Combobox.Options>
 
