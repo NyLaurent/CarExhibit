f8d739e46ee57bbc3e8fe78f6db671d6c878374a
NyLaurent <laulanyumbayire@gmail.com>
2024-12-27 20:04:56 -0600

    api's initialized

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+export async function fetchCars() 
+{
+    const headers ={
+        
+            'x-rapidapi-key': 'fa1b7ba3aamsh825ca39209b7192p15df6cjsn20ecc9c11792',
+            'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+        
+    }
+    const response =  await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',{
+        headers:headers,
+    });
+    const result = await response.json();
+    return result;
+    
+}
