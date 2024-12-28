10e70256c34d1e97832458634d680403578a9c29
NyLaurent <laulanyumbayire@gmail.com>
2024-12-27 18:32:19 -0600

    search functionality achieved

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -13,7 +13,13 @@ const SearchManufacturer =
     SearchManufacturerPops) => {
 
         const [query, setQuery] = useState('');
-        const filtere
+        const filteredManufacturers= query === "" ?
+        manufacturers :
+         manufacturers.filter((item)=>(
+            item.toLocaleLowerCase().replace(/\s+/g,"")
+            .includes(query.toLocaleLowerCase().replace(/\s+/g,""))
+
+        ))
   return (
     <div className='search-manufacturer'>
         <Combobox>
@@ -43,6 +49,26 @@ const SearchManufacturer =
                     afterLeave={() => setQuery('')}
                     >
                         <Combobox.Options>
+                            {filteredManufacturers.length=== 0 && query !=="" && query !== "" ?(
+                                <Combobox.Option
+                                value={query}
+                                className="search-manufacturer__option"
+                                >
+                                Create "{query}"</Combobox.Option>
+                            ):(
+                                filteredManufacturers.map((item)=>(
+                                    <Combobox.Option
+                                    key={item}
+                                    className={({active})=>`
+                                    relative search-manufacturer__option ${active ? 'bg-primary-blue text-white':'text-gray-900'}
+                                    `}
+                                    value={item}>
+                                        {item}
+
+                                    </Combobox.Option>
+
+                                ))
+                            ) }
 
                         </Combobox.Options>
 
