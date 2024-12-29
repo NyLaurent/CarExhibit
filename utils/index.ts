7849db6076533eb40ccd9f6388c1a202cf12d37f
NyLaurent <laulanyumbayire@gmail.com>
2024-12-29 13:42:19 -0600

    angles configured

--- utils/index.ts
+++ utils/index.ts
@@ -30,41 +30,6 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     return rentalRatePerDay.toFixed(0);
   };
 
-//   export const generateCarImageUrl=(car:CarProps, angle?: string)=>{
-// const url= new URL('https://cdn.imagin.studio/getimage')
-
-
-// const {make, year,model}= car;
-// url.searchParams.append('customer',
-//     'hrjavascript-mastery'
-// );
-// url.searchParams.append('make',make);
-// url.searchParams.append('modelFamily',model.split('')[0]);
-// url.searchParams.append('zoomType','fullscreen');
-// url.searchParams.append('modelYear',`${year}`);
-// url.searchParams.append('make',`${angle}`);
-
-
-// return `${url}`
-//   }
-
-
- 
-
-// export const generateCarImageUrl = (car: CarProps, angle?: string) => { const url = new URL("https://cdn.imagin.studio/getimage");
-
-//     url.searchParams.append("customer", "img");
-//      const { make, model, year } = car;
-//       url.searchParams.append("customer", "img");
-//        url.searchParams.append("make", make);
-//         url.searchParams.append("modelFamily", model.split(" ")[0]);
-//          url.searchParams.append("zoomType", "fullscreen"); 
-//          url.searchParams.append("modelYear", `${year}`);
-//          url.searchParams.append('make', `${angle}`);
-
-// return `${url}`;
-// };
-
 
 
 
