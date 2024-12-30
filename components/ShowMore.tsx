b7e27bd6372701f82817160bcbfb946113c00507
NyLaurent <laulanyumbayire@gmail.com>
2024-12-30 11:59:45 -0600

    sho more

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,13 +4,22 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { ShowMoreProps } from '@/types'
 import CustomButton from './CustomButton'
+import { updateSearchParams } from '@/utils'
 
 const ShowMore = ({pageNumber,isNext}:ShowMoreProps) => {
 
 
-    const router = useRouter;
+    const router = useRouter();
 
-    const handleNavigation=()=>{}
+    const handleNavigation=()=>{
+        const newLimit = (pageNumber +1) * 10;
+const newPathname = updateSearchParams("limit",
+    `${newLimit}`);
+   (newPathname);
+   router.push(newPathname,{scroll:false});
+
+
+ }
         return(
             <div className='w-full flex-center gap-5 mt-10'>
 
